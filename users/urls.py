0a41cb5fe82897553d520232ff3f8d5902f541fb
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-07-19 22:05:23 +0100

    urls.py

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+# users/urls.py
+
+from django.urls import path
+from .views import UserCreateView, UserDetailView
+
+urlpatterns = [
+    path('register/', UserCreateView.as_view(), name='user-register'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+]
