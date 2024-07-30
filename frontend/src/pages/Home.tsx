31ffb0df3e709a6c7b250afd6afa2ac1ae482481
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-07-30 08:20:16 +0100

    Home.tsx

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">Welcome to the Voting App</h1>
+    </div>
+  );
+};
+
+export default Home;
